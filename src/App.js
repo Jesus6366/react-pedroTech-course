b55a18a257abf1acaf6296fb275cc63104463c87
unknown <polibos6366@gmail.com>
2024-09-17 18:05:07 -0600

    States in React, useState Hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const helloWorld = "Hello world";
+  const [showText, setShowText] = useState(true);
+
   return (
     <div className="App">
-      <User name="jesus" age={34} email="polibos@gmail.com" />
-      <User name="sarai" age={33} email="sarai@gmail.com" />
-      <User />
-
-      <Job salary={90000} position={"Senior SDE"} company="amazon" />
-      <Job salary={12000} position={"Junior SDE"} company="Google" />
-      <Job salary={100000} position={"Manager"} company="Netflix" />
+      <button onClick={() => setShowText(!showText)}>Show/Hide</button>
+      {showText && <h1>Hi My name is jesus </h1>}
     </div>
   );
 }
-
-const User = ({ name, age, email }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{age}</h1>
-      <h1>{email}</h1>
-    </div>
-  );
-};
-
-const Job = ({ salary, position, company }) => {
-  return (
-    <div>
-      <h1>{salary}</h1>
-      <h1>{position}</h1>
-      <h1>{company}</h1>
-    </div>
-  );
-};
 
 export default App;
