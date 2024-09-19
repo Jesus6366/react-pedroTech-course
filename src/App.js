0cfe486522153fd1e13de1b64f704aa165285c28
unknown <polibos6366@gmail.com>
2024-09-18 18:57:51 -0600

    React Query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, createContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./App.css";
 import Home from "./pages/Home";
@@ -13,19 +14,23 @@ export const AppContext = createContext();
 function App() {
   const [username, setUsername] = useState("Jesus");
 
+  const client = new QueryClient();
+
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>Page not found</h1>} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ username, setUsername }}>
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<h1>Page not found</h1>} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../App.js";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
-  const { username } = useContext(AppContext);
-  return <h1>This is the home page and user is: {username}</h1>;
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ["cat"],
+    queryFn: async () => {
+      const res = await fetch("https://catfact.ninja/fact");
+      return res.json();
+    },
+  });
+
+  // Handle loading state
+  if (isLoading) return <h1>Loading...</h1>;
+
+  // Handle error state
+  if (error) return <h1>Error: {error.message}</h1>;
+
+  return (
+    <h1>
+      This is the home page <p>{data?.fact}</p>
+      <button onClick={refetch}>Update</button>
+    </h1>
+  );
 };
 
 export default Home;
